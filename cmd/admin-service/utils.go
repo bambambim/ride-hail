9760d6e2c5c2a1792807dfc8f69f653bc917fb29
bambambim <dan4ik2006@mail.ru>
2025-10-29 19:38:25 +0500

    feat: admin service

---
+++ cmd/admin-service/utils.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+)
+
+func writeJSON(w http.ResponseWriter, status int, v interface{}) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	if v == nil {
+		return nil
+	}
+	return json.NewEncoder(w).Encode(v)
+}
+
+func writeError(w http.ResponseWriter, code int, msg string) {
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+	writeJSON(w, code, errResponse{Error: msg})
+}
+
+func parsePagination(r *http.Request) (page int, pageSize int) {
+	pageStr := r.URL.Query().Get("page")
+	pageSizeStr := r.URL.Query().Get("pageSize")
+	var err error
+	page, err = strconv.Atoi(pageStr)
+	if err != nil || page < 1 {
+		page = 1
+	}
+	pageSize, err = strconv.Atoi(pageSizeStr)
+	if err != nil || pageSize < 1 || pageSize > 100 {
+		pageSize = 10
+	}
+	return page, pageSize
+}
